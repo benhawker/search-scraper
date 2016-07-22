553f834022896a16c4e05625a28370a39173bc23
benhawker <ben.c.hawker@gmail.com>
2016-07-22 17:01:05 +0800

    Create client class

---
+++ lib/tujia_scraper.rb
@@ -0,0 +1,4 @@
+require "mechanize"
+require "csv"
+
+require_relative "tujia_scraper/client"
---
+++ lib/tujia_scraper/client.rb
@@ -0,0 +1,17 @@
+module TujiaScraper
+  class Client
+
+    BASE_URL = "http://tujia.com/"
+
+    attr_reader :destination
+
+    def initialize(destination)
+      @destination = destination
+    end
+
+    private
+
+
+
+  end
+end
