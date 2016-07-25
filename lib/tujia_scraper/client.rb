e771700938aa3218819765567461f0873a5c7e8f
benhawker <ben.c.hawker@gmail.com>
2016-07-25 10:35:00 +0800

    Suucessfully calling url and returning

--- lib/tujia_scraper/client.rb
+++ lib/tujia_scraper/client.rb
@@ -1,17 +1,47 @@
 module TujiaScraper
   class Client
 
-    BASE_URL = "http://tujia.com/"
+    BASE_URL = "http://international.tujia.com/"
 
-    attr_reader :destination
+    CITIES = {
+               london: "lundun_gongyu_r17/",
+               singapore: "xinjiapo_gongyu_r104/",
+               tokyo: "dongjing_gongyu_r77/",
+               osaka: "daban_gongyu_r147/"
+             }
 
-    def initialize(destination)
-      @destination = destination
+    attr_reader :city, :start_date, :end_date, :guest_count
+
+    def initialize(city, start_date, end_date, guest_count)
+      @city = city
+      @start_date = start_date
+      @end_date = end_date
+      @guest_count = guest_count
+    end
+
+    def scrape
+      http_client.get(url_builder)
     end
 
     private
 
+    def http_client
+      Mechanize.new
+    end
 
+    def url_builder
+      url = BASE_URL + CITIES[city.to_sym]
+      url += params_builder if params?
+      url
+    end
+
+    def params?
+      start_date && end_date && guest_count
+    end
+
+    def params_builder
+      "?startDate=#{start_date}&endDate=#{start_date}&guestCount=#{guest_count}"
+    end
 
   end
 end
